5766818f2529d2d913ed2371722900876c9ef5c9
j342335 <j342335@gmail.com>
2021-11-02 22:40:20 +0800

    Update client.c

--- hw3/client.c
+++ hw3/client.c
@@ -7,7 +7,7 @@
 
 const int SERV_PORT = 65535;
 const int MAXLINE = 2048;
-
+//使用sentto()傳送資料，使用recvfrom()進入等待狀態，直到接收到server程式傳送的資料，就處理收到的資料。
 void dg_cli(FILE *fp , int sockfd , const struct sockaddr *pservaddr , socklen_t servlen)
 {
 	int n;
@@ -37,7 +37,7 @@ int main(void)
 {
 	int sockfd , t;
 	struct sockaddr_in servaddr;
-
+	//初始化sockaddr_in結構的變數，並賦值
 	bzero(&servaddr , sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(SERV_PORT);
@@ -46,13 +46,13 @@ int main(void)
 		perror("inet_pton error");
 		exit(1);
 	}
-	
+	//使用socket()來建立一個UDP socket，第二個引數為SOCK_DGRAM
 	if((sockfd = socket(AF_INET , SOCK_DGRAM , 0)) < 0)
 	{
 		perror("socket error");
 		exit(1);
 	}
-
+	//進入無限迴圈程式，dg_cli
 	dg_cli(stdin , sockfd , (struct sockaddr *)&servaddr , sizeof(servaddr)) ;
 	exit(0);
 } 
